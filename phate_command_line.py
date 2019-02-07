2158e16a9402fa5b69f84f4c951f275d18a9447c
Scott Gigante <scottgigante@gmail.com>
2019-02-07 10:45:49 -0500

    improve error messages, fix validate

--- phate_command_line.py
+++ phate_command_line.py
@@ -157,9 +157,9 @@ def parse_args():
 
     if args.validate:
         tasklogger.set_level(2)
-        tasklogger.log_info("Running MAGIC validation.")
+        tasklogger.log_info("Running PHATE validation.")
         args.filename = "https://github.com/KrishnaswamyLab/scprep/raw/master/data/test_data/test_small.csv"
-        args.sparse = True
+        args.sparse = False
         args.gene_names = True
         args.cell_names = True
         args.cell_axis = "row"
@@ -218,36 +218,36 @@ def parse_args():
             args.gene_names = None
         else:
             parser.error(
-                "Cannot handle --gene-names with {} file".format(filetype[1:]))
+                "Cannot handle --gene-names with {} file".format(filetype))
         if '--cell-names' not in sys.argv:
             args.cell_names = None
         else:
             parser.error(
-                "Cannot handle --cell-names with {} file".format(filetype[1:]))
+                "Cannot handle --cell-names with {} file".format(filetype))
     if filetype not in ['csv', 'tsv', 'csv.gz', 'tsv.gz', 'mtx']:
         if '--cell-axis' not in sys.argv:
             args.cell_axis = None
         else:
             parser.error(
-                "Cannot handle --cell-axis with {} file".format(filetype[1:]))
+                "Cannot handle --cell-axis with {} file".format(filetype))
     if filetype not in ['dir', 'zip', 'hdf5', 'h5']:
         if '--gene-labels' not in sys.argv:
             args.gene_labels = None
         else:
             parser.error(
-                "Cannot handle --gene-labels with {} file".format(filetype[1:]))
+                "Cannot handle --gene-labels with {} file".format(filetype))
     if filetype not in ['hdf5', 'h5']:
         if '--genome' not in sys.argv:
             args.genome = None
         else:
             parser.error(
-                "Cannot handle --genome with {} file".format(filetype[1:]))
+                "Cannot handle --genome with {} file".format(filetype))
     if filetype not in ['fcs']:
         if '--metadata-channels' not in sys.argv:
             args.metadata_channels = None
         else:
             parser.error(
-                "Cannot handle --metadata-channels with {} file".format(filetype[1:]))
+                "Cannot handle --metadata-channels with {} file".format(filetype))
 
     # check for inappropriately set parameters
     if not args.transform == 'log':
